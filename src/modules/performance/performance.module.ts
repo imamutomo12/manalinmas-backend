1e0060e47514e3639ad258016c8060ee57d42f9f
imamutomo12 <imamutomo69@gmail.com>
2026-08-08 19:25:44 +0700

    feat-fix performance module storage import

--- src/modules/performance/performance.module.ts
+++ src/modules/performance/performance.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PerformanceController } from './performance.controller';
 import { PerformanceService } from './performance.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StorageModule],
   controllers: [PerformanceController],
   providers: [PerformanceService],
   exports: [PerformanceService],
